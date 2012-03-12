8d24a72f56d4577a7bc234c1df82715598662b12
unknown <JT@jtruscott.(none)>
2012-03-12 16:04:59 -0700

    Cleaned up to be respectable

--- statistics.py
+++ statistics.py
@@ -7,8 +7,10 @@ import sys
 import os
 import pickle
 import csv
+from operator import itemgetter
 
-
+#load a bunch of data files
+#csv's helpfully provided by the veekun/pokedex project.
 pokemon = [
 	(int(number), name) for number, sep, name in
 		[a.partition(',') for a in 
@@ -27,8 +29,9 @@ pokemon_species = dict([
  	in list(csv.reader(open('csv/pokemon_species.csv')))[1:]
 ])
 def evolution_level(pokemon_id):
+	#it's a little annoying, but determining evolutionary stage
+	#requires recursive-walking the chain
 	evolves_from, is_baby = pokemon_species[pokemon_id]
-	#print pokemon_id, evolves_from, is_baby
 	if not evolves_from:
 		if is_baby:
 			return 'baby'
@@ -39,6 +42,17 @@ def evolution_level(pokemon_id):
 	if next == 'stage1':
 		return 'stage2'
 	return 'stage1'
+
+def generation_name(pokemon_id):
+	if number >= 493:
+		return 'V'
+	if number >= 386:
+		return 'IV'
+	if number >= 251:
+		return 'III'
+	if number >= 151:
+		return 'II'
+	return 'I'
 	
 pokemon_stats = dict()
 for pokemon_id,stat_id,base_stat,effort in list(csv.reader(open('csv/pokemon_stats.csv')))[1:]:
@@ -48,37 +62,71 @@ for pokemon_id,stat_id,base_stat,effort in list(csv.reader(open('csv/pokemon_sta
 
 stat_list = sorted(pokemon_stats.values())
 #--
+class site(object):
+	translations = {
+		'nidoranf': 'nidoran%E2%99%80',
+		'nidoranm': 'nidoran%E2%99%82',
+		'hooh': 'ho-oh',
+		'farfetchd': "farfetch'd",
+		'mrmime': 'mr._mime',
+		'mimejr': 'mime_jr.',
+		'porygonz': 'porygon-z',
+	}
+	def __init__(self):
+		self.pokemon_counts = {}
+		self.total_count = 0
 
-e6URL = base64.b64decode('aHR0cDovL2U2MjEubmV0L3Bvc3Q=')
-wcURL = base64.b64decode('aHR0cDovL3dpbGRjcml0dGVycy53cy9wb3N0')
-r34URL = base64.b64decode('aHR0cDovL3J1bGUzNC54eHgvaW5kZXgucGhw')
+	def count_results(self, term, data):
+		if term in self.translations:
+			term = self.translations[term]
+		ret = re.findall('%s.*?"post-count">(\d+)</span>' % term, data)
+		if not ret:
+			return 0
+		return int(ret[0])
 
-def e6(query):
-	return e6URL + '?tags=%s&commit=Search' % urllib2.quote(query)
+	def translate(self, term):
+		#Turn a name into a URL term
+		#and fixup any site-specific peculiarities
+		if term in self.translations:
+			return self.translations[term]
+		return urllib2.quote(term)
 
-def wc(query):
-	return wcURL + '?tags=%s' % urllib2.quote(query)
+	def get_url(self, term):
+		return self.url % self.translate(term)
 
-def r34(query):
-	return r34URL + '?page=post&s=list&tags=%s' % urllib2.quote(query)
+	def get(self, term):
+		data = urllib2.urlopen(self.get_url(term)).read()
+		return data
 
+class e621(site):
+	url = 'http://e621.net/post?tags=%s+rating:explicit&commit=Search'
+	data_file_name = 'e6.pickle'
+
+class wildcritters(site):
+	url = 'http://wildcritters.ws/post?tags=%s+rating:explicit'
+	data_file_name = 'wc.pickle'
+
+class rule34(site):
+	url = 'http://rule34.xxx/index.php?page=post&s=list&tags=%s'
+	data_file_name = 'r34.pickle'
+	
+	def count_results(self, term, data):
+		if term in self.translations:
+			term = self.translations[term]
+		ret = re.findall('>%s<.*?>(\d+)<' % term, data)
+		if not ret:
+			return 0
+		return int(ret[0])
+
+sites = dict(
+	e621=e621(),
+	wildcritters=wildcritters(),
+	rule34=rule34(),
+)
+site_list = sites.values()
 #--
 
-def count_results(query, data):
-	ret = re.findall('%s.*?"post-count">(\d+)</span>' % query, data)
-	if not ret:
-		return 0
-	return int(ret[0])
 
-def r34_count_results(query, data):
-	ret = re.findall('>%s<.*?>(\d+)<' % query, data)
-	if not ret:
-		return 0
-	return int(ret[0])
-
-def get(query):
-	data = urllib2.urlopen(r34(query)).read()
-	return data
 
 def median(values):
 	v = sorted(values)
@@ -89,117 +137,112 @@ def median(values):
 	
 if __name__ == '__main__':
 	if 'fetch' in sys.argv:
-		pages = {}
+		key = sys.argv[-1]
+		site = sites[key]
+		pages = dict([(key, {}) for key in sites.keys()])
 		try:
 			for number, name in pokemon:
-				print number, name
-				pages[number] = get(name)
+				print number, name,
+				pages[key][number] = site.get(name)
+				print
 		finally:
-			pickle.dump(pages, open('data.pickle', 'w'))
+			pickle.dump(pages[key], open(site.data_file_name, 'w'))
 	else:
-		wc_data =  pickle.load(open('wc.pickle'))
-		e6_data =  pickle.load(open('e6.pickle'))
-		r34_data =  pickle.load(open('r34.pickle'))
-		counts = []
-		typecounts = {}
-		gencounts = {}
-		stagecounts = {}
-		percentilecounts = {}
-		allcounts = []
+		#Unpickle the html data
+		#and prepare counts by pokemon so we can get a total count
+		print 'Loading data'
+		for site in site_list:
+			site.data = pickle.load(open(site.data_file_name))
+			for number, name in pokemon:
+				site.pokemon_counts[number] = site.count_results(name, site.data[number])
+			site.total_count = float(sum(site.pokemon_counts.values()))
+		interest_list = []
+		interest_map = {}
 
-		e6_counts = {}
-		wc_counts = {}
-		r34_counts = {}
+		#For each pokemon, generate an "Interest Score" using the following method:
+		#Take the number of hits per site, divided by site size, average those values,
+		#then scale relative to the median value (which will become '100%')
+		#This expresses the pokemon's value as a percentage-of-the-median.
 		for number, name in pokemon:
-			e6_counts[number] = count_results(name, e6_data[number])
-			wc_counts[number] = count_results(name, wc_data[number])
-			r34_counts[number] = r34_count_results(name, r34_data[number])
-
-		total_e6_count = float(sum([x for x in e6_counts.values()]))
-		total_wc_count = float(sum([x for x in wc_counts.values()]))
-		total_r34_count = float(sum([x for x in r34_counts.values()]))
-
-		count_map = {}
-
-		for number, name in pokemon:
-			e6_count, wc_count, r34_count = e6_counts[number], wc_counts[number], r34_counts[number]
-			best_count = sum((e6_count/total_e6_count, wc_count/total_wc_count, r34_count/total_r34_count))/3
-			count_map[number] = best_count
-			allcounts.append(best_count)
+			interest_scores = [site.pokemon_counts[number] / site.total_count for site in site_list]
+			interest_score = sum(interest_scores) / len(interest_scores)
+			interest_map[number] = interest_score
+			interest_list.append(interest_score)
 		
+		median_interest = median(interest_list)
 
+		for number, interest_score in interest_map.items():
+			interest_map[number] = interest_score / median_interest * 100
 
-		mediancount = median(allcounts)
+		interest_list = [interest_score / median_interest * 100 for interest_score in interest_list]
 
-		for number, best_count in count_map.items():
-			count_map[number] = best_count / mediancount * 100
+		#Now that we have that, start building up rows for analysis!
+		print 'Creating rows'
+		rows = []
 
 		for number, name in pokemon:
-			best_count = count_map[number]
-			e6_count, wc_count, r34_count = e6_counts[number], wc_counts[number], r34_counts[number]
-			#male_count = max(count_results('=male', e6_data[number]), count_results('%E2%99%82', wc_data[number]))
-			#female_count = max(count_results('=female', e6_data[number]), count_results('%E2%99%80', wc_data[number]))
+			interest_score = interest_map[number]
+			
+			#get the keys for various attributes
 			stage = evolution_level(number)
-			percentile = int(float(stat_list.index(pokemon_stats[number])) / len(stat_list) * 10)
-
-			#print number, name, evolution_level(number)
-
-			gender_ratio = '' #'%.1f%%' % ((float(male_count) / max(male_count + female_count, 1)) * 100)
-
+			stat_percentile = int(float(stat_list.index(pokemon_stats[number])) / len(stat_list) * 10)
+			stat_percentile = '%i0%%-%i0%%' % (9-stat_percentile, 10-stat_percentile)
+			generation = generation_name(number)
 			type1 = typedict.get((number,1),'')
 			type2 = typedict.get((number, 2), '')
-			if type1:
-				typecounts.setdefault(type1, []).append(best_count)
-			if type2:
-				typecounts.setdefault(type2, []).append(best_count)
-			generation = 'I'
-			if number >= 151:
-				generation = 'II'
-				if number >= 251:
-					generation = 'III'
-					if number >= 386:
-						generation = 'IV'
-						if number >= 493:
-							generation = 'V'
 
-			gencounts.setdefault(generation, []).append(best_count)
-			stagecounts.setdefault(stage, []).append(best_count)
-			percentilecounts.setdefault(percentile, []).append(best_count)
+			row = dict(
+				interest_score=interest_score,
+				number=number,
+				name=name,
+				type1=type1,
+				type2=type2,
+				stage=stage,
+				stat_percentile=stat_percentile,
+				generation=generation,
+			)
+			for site_name, site in sites.items():
+				row[site_name] = site.pokemon_counts[number]
 
-			counts.append([
-				best_count, e6_count, wc_count, r34_count, number, gender_ratio, name, type1, type2, stage
-			])
-		#print counts[:10]
-		counts = sorted(counts, reverse=True)
+			rows.append(row)
 
+		rows = sorted(rows, key=itemgetter('interest_score'), reverse=True)
 
-		typecounts = sorted([(median(items),items,name) for name,items in typecounts.items()], reverse=True)
-		gencounts = sorted([(median(items),items,name) for name,items in gencounts.items()], reverse=True)
-		stagecounts = sorted([(median(items),items,name) for name,items in stagecounts.items()], reverse=True)
-		percentilecounts = sorted([(median(items),items,name) for name,items in percentilecounts.items()], reverse=True)
-		#print typecounts
+		print "Creating breakdowns"
+		#build the grouped breakdown info
+		breakdowns = {}
+		scale_maxes = {}
+		for breakdown_key in ('generation', 'stat_percentile', 'stage', 'type'):
+			data_map = {}
+			scale_max = 0
+			for row in rows:
+				if breakdown_key == 'type':
+					#types are merged from two fields
+					data_map.setdefault(row['type1'], []).append(row['interest_score'])
+					if row['type2']:
+						data_map.setdefault(row['type2'], []).append(row['interest_score'])
+					
+				else:
+					map_key = row[breakdown_key]
+					data_map.setdefault(map_key, []).append(row['interest_score'])
 
-		
-		#type_writer = csv.writer(open('typecounts.csv', 'w'))
-		#type_writer.writerows(typecounts)
-		#writer = csv.writer(open('counts.csv', 'w'))
-		#writer.writerows(counts)
-		print 'Creating HTML'
+				scale_max = max(scale_max, row['interest_score'])
+			breakdowns[breakdown_key] = sorted([
+				(median(scores), scores, map_key) for map_key, scores in data_map.items()
+			], reverse=True)
+
+			scale_maxes[breakdown_key] = scale_max
+
+		print 'Creating html'
 		from jinja2 import Template
 		with open('tables.html', 'w') as out:
 			out.write(Template(open('html.template').read()).render(
-				counts=counts,
-				typecounts=typecounts,
-				gencounts=gencounts,
-				stagecounts=stagecounts,
-				percentilecounts=percentilecounts,
-				allcounts=allcounts,
-				typemax = max([max(i) for _,i,_ in typecounts]),
-				genmax = max([max(i) for _,i,_ in gencounts]),
-				stagemax = max([max(i) for _,i,_ in stagecounts]),
-				percentilemax = max([max(i) for _,i,_ in percentilecounts]),
-				mediancount = mediancount,
+				rows=rows,
+				breakdowns=breakdowns,
+				scale_maxes=scale_maxes,
+				interest_list = interest_list,
 
-				min=min,max=max,e6=e6,wc=wc,r34=r34
+				sites=sites,
+				min=min,max=max,
 			))
 		print 'Done'
